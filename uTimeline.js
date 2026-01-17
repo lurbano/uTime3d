866fcdd753acde9cc00a739971c393ad30a47a45
lurbano <lurbano@earthsciweb.org>
2026-01-17 15:39:06 -0600

    2d timelines WIP

--- uTimeline.js
+++ uTimeline.js
@@ -12,6 +12,36 @@ class uTimeline {
         this.blocks = this.params.blocks;
     }
 
+    addControls(divId=""){
+        console.log(divId)
+        this.controlsElement = document.getElementById(divId);
+        this.eventsList = [];
+        this.periodsList = [];
+
+        this.controlsElement.setAttribute("display", 'grid');
+        this.controlsElement.setAttribute("grid-template-columns", "1fr 1fr");
+        this.controlsElement.setAttribute("gap", "5px");
+
+        this.eventsListArea = document.createElement("div");
+        this.periodsListArea = document.createElement("div");
+
+        this.controlsElement.appendChild(this.eventsListArea);
+        this.controlsElement.appendChild(this.periodsListArea);
+
+        console.log(this.controlsElement);
+
+        //startTime input
+        // let startEvent = new timelineEvent(-1000, "StartTime");
+        // let endEvent = new timelineEvent(0, "End Time");
+
+        // full period
+        let fullTime = new timelinePeriod(-4.5, 0, "Earth History");
+        this.periodsListArea.appendChild(fullTime.makeHtmlInputs());
+        this.periodsList.push(fullTime);
+
+
+    }
+
     draw2d(divId="", params={}){
         let defaults = {
             width: 1000,
@@ -23,6 +53,49 @@ class uTimeline {
     
     }
 }
+
+class timelineEvent {
+    constructor(eventTime, description){
+
+    }
+}
+
+class timelinePeriod {
+    constructor(startTime, endTime, description){
+        this.startTime = startTime;
+        this.endTime = endTime;
+        this.description = description;
+    }
+    makeHtmlInputs(){
+        this.inputBlock = document.createElement("div");
+        this.inputBlock.style.width = "95%";
+        this.inputBlock.style.backgroundColor = "lightSalmon";
+        this.inputBlock.style.padding = "2px";
+
+        this.startInput = document.createElement('input');
+        this.startInput.setAttribute("type", "number");
+        this.startInput.setAttribute("value", this.startTime);
+        this.inputBlock.appendChild(this.startInput);
+
+        this.endInput = document.createElement('input');
+        this.endInput.setAttribute("type", "number");
+        this.endInput.setAttribute("value", this.endTime);
+        this.inputBlock.appendChild(this.endInput);
+
+        this.descInput = document.createElement('input');
+        this.descInput.setAttribute("type", "text");
+        this.descInput.setAttribute("value", this.description);
+        this.inputBlock.appendChild(this.descInput);
+
+        this.delButton = document.createElement("input");
+        this.delButton.setAttribute('type', "button");
+        this.delButton.setAttribute('value', "-");
+        this.inputBlock.appendChild(this.delButton);
+
+        return this.inputBlock;
+    }
+}
+
 
 class svgTimeline {
     constructor(divId="", params={}){
@@ -50,5 +123,26 @@ class svgTimeline {
         console.log(this.element.getAttribute("width"));
         this.parentElement.appendChild(this.element);
         
+    }
+
+    drawPeriod(){
+        
+    }
+}
+
+class divTimeline {
+    constructor(divId="", params={}){
+        let defaults = {
+            width: 1000,
+            height: 20, 
+            fill: "lightblue",
+            stroke: "red",
+            "stroke-width": "2"
+        }
+        //this.params = {...defaults, ...params};
+        this.parentElement = document.getElementById(divId);
+        this.element = document.createElement('div');
+
+        this.parentElement.appendChild(this.element);
     }
 }
